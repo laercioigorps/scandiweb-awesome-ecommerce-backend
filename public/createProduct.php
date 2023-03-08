961015e50c54e2cec803bcecd0ef3177f4d13c42
Laercio Souza <laercioigorps@gmail.com>
2023-03-08 17:22:02 -0300

    add cors origins header

--- public/createProduct.php
+++ public/createProduct.php
@@ -2,5 +2,8 @@
 echo '{"created":"true"}';
 http_response_code(201);
 
+
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Content-Type: application/json; charset=utf-8');
