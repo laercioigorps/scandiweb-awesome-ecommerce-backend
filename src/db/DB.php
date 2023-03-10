7e547e4b27436840c94041d981e88f7610b80366
Laercio Souza <laercioigorps@gmail.com>
2023-03-10 14:59:30 -0300

    update DB to be an Object and ProductDBManager to connect to it

--- src/db/DB.php
+++ src/db/DB.php
@@ -1,19 +1,19 @@
 <?php
 
-function getConnection()
+class DB
 {
-    $servername = "127.0.0.1";
-    $database = "products";
-    $username = "root";
-    $password = "password";
-    $charset = "utf8mb4";
-    try {
-        $dsn = "mysql:host=$servername;dbname=$database;charset=$charset";
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-    }
 
+    private $servername = "127.0.0.1";
+    private $database = "products";
+    private $username = "root";
+    private $password = "password";
+    private $charset = "utf8mb4";
+
+    public function getConnection()
+    {
+        $dsn = "mysql:host=$this->servername;dbname=$this->database;charset=$this->charset";
+        $pdo = new PDO($dsn, $this->username, $this->password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
 }
--- src/db/ProductDBManager.php
+++ src/db/ProductDBManager.php
@@ -3,13 +3,17 @@ require('DB.php');
 
 class ProductDBManager
 {
-    public static function getAll(): PDOStatement|bool
+    public static function getAll(): PDOStatement
     {
-        $con = getConnection();
-        $data = $con->query('
-SELECT id, sku, name, price, type, height, width, length, null as size, null as weight FROM product_furniture Union 
-SELECT id, sku, name, price, type, null, null, null, size, null FROM product_dvd Union
-SELECT id, sku, name, price, type, null, null, null, null, weight FROM product_book;');
+        $db = new DB();
+        $connection = $db->getConnection();
+        if ($connection) {
+            $data = $connection->query('
+            SELECT id, sku, name, price, type, height, width, length, null as size, null as weight FROM product_furniture Union 
+            SELECT id, sku, name, price, type, null, null, null, size, null FROM product_dvd Union
+            SELECT id, sku, name, price, type, null, null, null, null, weight FROM product_book;');
+        }
+
         return $data;
     }
 
