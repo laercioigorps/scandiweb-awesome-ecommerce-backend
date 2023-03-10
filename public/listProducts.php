ef49f883ecf867a6e255d2e7dfb072623a4b346e
Laercio Souza <laercioigorps@gmail.com>
2023-03-10 14:22:54 -0300

    update page to use ProductDMManager

--- public/listProducts.php
+++ public/listProducts.php
@@ -1,12 +1,8 @@
 <?php
+require('../src/db/ProductDBManager.php');
 
-require('../src/db/DB.php');   
+$data = ProductDBManager::getAll();
 
-$con = getConnection();
-$data = $con->query('
-SELECT id, sku, name, price, type, height, width, length, null as size, null as weight FROM product_furniture Union 
-SELECT id, sku, name, price, type, null, null, null, size, null FROM product_dvd Union
-SELECT id, sku, name, price, type, null, null, null, null, weight FROM product_book;');
 $main_list = [];
 
 foreach ($data as $row) {
