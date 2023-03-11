c925cc30baaa1ccd0785bfd07d4b175b16d0ebeb
Laercio Souza <laercioigorps@gmail.com>
2023-03-11 05:58:51 -0300

    update index.php to redirect to controler class

--- public/index.php
+++ public/index.php
@@ -1,12 +1,20 @@
 <?php
+require_once('../src/controlers/ProductControler.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
-    '/products' => 'listProducts.php',
-    '/products/create' => 'createProduct.php',
+    '/products' => function () {
+        $controler = new ProductControler();
+        $controler->list();
+    },
+    '/products/create' => function () {
+        $controler = new ProductControler();
+        $controler->create();
+    },
 ];
 
 if (array_key_exists($uri, $routes)) {
-    require($routes[$uri]);
-};
+    $routes[$uri]();
+}
+;
