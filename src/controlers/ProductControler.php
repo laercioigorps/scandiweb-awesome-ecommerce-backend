f784f7c9a154d16c2281e350b54df873c56fb79b
Laercio Souza <laercioigorps@gmail.com>
2023-03-12 07:16:52 -0300

    update ProductControler to use ProductSerializerFactory

--- src/controlers/ProductControler.php
+++ src/controlers/ProductControler.php
@@ -1,42 +1,40 @@
 <?php
 
+require_once('../src/services/ProductSerializerFactory.php');
+require_once('../src/services/ProductFactory.php');
 require('../src/db/ProductDVDDBManager.php');
 require('../src/db/ProductBookDBManager.php');
 require('../src/db/ProductFurnitureDBManager.php');
 require_once('../src/db/ProductDBManager.php');
-require_once('../src/serializers/ProductDVDSerializer.php');
-require_once('../src/serializers/ProductFurnitureSerializer.php');
-require_once('../src/serializers/ProductBookSerializer.php');
 
 class ProductControler
 {
     public function create()
     {
+        header('Content-type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if ($_SERVER['REQUEST_METHOD'] != "POST") {
+            die();
+        }
+
         // Converts it into a PHP object
         $data = json_decode(file_get_contents('php://input'), true);
-        $type = $data['type'];
-        if ($type === 'dvd') {
-            $serializer = new ProductDVDSerializer($data);
-            if ($serializer->isValid()) {
-                ProductDVDDBManager::create($serializer->getCleanedData());
-            }
-            //createProductDVD($data);
-        } else if ($type === 'furniture') {
-            $serializer = new ProductFurnitureSerializer($data);
-            if ($serializer->isValid()) {
-                ProductFurnitureDBManager::create($data);
-            }
-        } else if ($type === 'book') {
-            $serializer = new ProductBookSerializer($data);
-            if ($serializer->isValid()) {
-                ProductBookDBManager::create($data);
-            }
+        $serializer = ProductSerializerFactory::getProductSerializer($data);
+        if ($serializer->isValid()) {
+            $product = ProductFactory::getProduct($serializer->getCleanedData());
         }
-        ;
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: POST");
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-        header('Content-Type: application/json; charset=utf-8');
+        if ($product->getType() === 'dvd') {
+            ProductDVDDBManager::create($data);
+            echo var_dump($product);
+        } elseif ($product->getType() === 'furniture') {
+            ProductFurnitureDBManager::create($data);
+        } elseif ($product->getType() === 'book') {
+            ProductBookDBManager::create($data);
+        }
+
         http_response_code(201);
     }
 
