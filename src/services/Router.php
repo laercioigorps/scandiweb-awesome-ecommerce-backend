28fd6db7e77722dfa8f441e0f15a1bb618f611b1
Laercio Souza <laercioigorps@gmail.com>
2023-03-13 14:05:04 -0300

    create basic Router class

---
+++ src/services/Router.php
@@ -0,0 +1,35 @@
+<?php
+class Router
+{
+
+    private $routes = [];
+
+    private function add($uri, $controler, $method)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'method' => $method,
+            'controler' => $controler,
+        ];
+    }
+
+    public function get($uri, $controler)
+    {
+        $this->add($uri, $controler, 'GET');
+    }
+
+    public function post($uri, $controler)
+    {
+        $this->add($uri, $controler, 'POST');
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $rout) {
+            if ($uri === $rout['uri'] && $method == $rout['method']) {
+                return $rout['controler']();
+            }
+        }
+        http_response_code(404);
+    }
+}
