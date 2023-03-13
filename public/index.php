84f39ebe10ef2d2e7bbd54e89faa9fcb439cc564
Laercio Souza <laercioigorps@gmail.com>
2023-03-13 14:05:43 -0300

    update index.html to use Router object

--- public/index.php
+++ public/index.php
@@ -1,28 +1,26 @@
 <?php
 require_once('../src/controlers/ProductControler.php');
+require_once('../src/services/Router.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = [
-    '/products' => function () {
-        $controler = new ProductControler();
-        $controler->list();
-    },
-    '/products/create' => function () {
-        $controler = new ProductControler();
-        $controler->create();
-    },
-];
+$router = new Router();
 
-if (array_key_exists($uri, $routes)) {
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-    if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
-        die();
-    }
-    $routes[$uri]();
+if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
+    die();
 }
-;
+
+$router->get('/products', function () {
+    $controler = new ProductControler();
+    $controler->list();
+});
+
+$router->post('/products/create', function () {
+    $controler = new ProductControler();
+    $controler->create();
+});
+$router->route($uri, $_SERVER['REQUEST_METHOD']);
