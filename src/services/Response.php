5f3dd2561db58c0d69235da3a0a0e6618cf95a7c
Laercio Souza <laercioigorps@gmail.com>
2023-03-13 16:03:10 -0300

    create basic response object

---
+++ src/services/Response.php
@@ -0,0 +1,34 @@
+<?php
+class Response
+{
+
+    private $data = null;
+    public function __construct($data = null, $status = 200)
+    {
+        $this->setHeaders();
+        $this->setStatus($status);
+        $this->setData($data);
+    }
+
+    private function setHeaders()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    }
+
+    public function setStatus($status)
+    {
+        http_response_code($status);
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+}
