60f6d0266fbe9f3154807ff06ef1f8ad9a36686e
Laercio Souza <laercioigorps@gmail.com>
2023-03-13 16:30:00 -0300

    removed not used code

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require_once('../src/services/Router.php');
 require_once('../src/services/Response.php');
 require_once('../src/services/Request.php');
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $request = new Request();
 $router = new Router();
 
