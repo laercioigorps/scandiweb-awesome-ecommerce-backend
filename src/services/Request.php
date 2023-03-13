be5d4dca7a40fec057bb7cdd27a831f56ae32bef
Laercio Souza <laercioigorps@gmail.com>
2023-03-13 16:28:49 -0300

    update Request class to have POST atribute

--- src/services/Request.php
+++ src/services/Request.php
@@ -4,11 +4,13 @@ class Request
 
     private $uri;
     private $method;
+    public $POST;
 
     public function __construct()
     {
         $this->uri = parse_url($_SERVER['REQUEST_URI'])['path'];
         $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->POST = json_decode(file_get_contents('php://input'), true);
     }
 
     public function getURI()
