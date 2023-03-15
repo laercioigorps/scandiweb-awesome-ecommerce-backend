83394e63f77b1bbd9b4c405756b8c87d58e96334
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 14:51:12 -0300

    create DecimalFieldValidator

---
+++ src/serializers/validators/DecimalFieldValidator.php
@@ -0,0 +1,34 @@
+<?php
+require_once('FieldValidator.php');
+class DecimalFieldValidator extends FieldValidator
+{
+    private $positive;
+
+    public function __construct($positive = false, $required = true, $unique = false, $uniqueChecker = null)
+    {
+        parent::__construct(required: $required, unique: $unique, uniqueChecker: $uniqueChecker);
+        $this->positive = $positive;
+    }
+
+    private function checkValidNumber($data)
+    {
+        if (!is_numeric($data)) {
+            $this->appendErrorMessage("Field must be a valid number!");
+        }
+    }
+
+    private function checkPositiveNumber($data)
+    {
+        if ($this->positive && is_numeric($data) && $data < 0) {
+            $this->appendErrorMessage("Field can not be a negative number!");
+        }
+    }
+
+    public function validate($data)
+    {
+        parent::validate($data);
+        $this->checkValidNumber($data);
+        $this->checkPositiveNumber($data);
+    }
+
+}
