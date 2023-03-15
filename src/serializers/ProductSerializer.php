ab7c9dbb6a60b28cda6d2710fc0cb94192a34188
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 14:19:18 -0300

    add a abstract class ProductSerializer that extends Serializer

---
+++ src/serializers/ProductSerializer.php
@@ -0,0 +1,16 @@
+<?php
+require_once('Serializer.php');
+abstract class ProductSerializer extends Serializer{
+
+    public function __construct($data = null, $instance = null)
+    {
+        parent::__construct(data: $data, instance: $instance);
+        $this->fields["name"] = new CharFieldValidator(maxLength: 50, required: true);
+        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true, unique: true, uniqueChecker: function ($sku) {
+            return ProductDBManager::isValidNewSKU($sku);
+        });
+        $this->fields["name"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["price"] = new FieldValidator(required: true);
+        $this->fields["type"] = new CharFieldValidator(maxLength: 25, required: true);
+    }
+}
