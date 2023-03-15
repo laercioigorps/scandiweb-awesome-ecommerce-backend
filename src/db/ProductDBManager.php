de473f2ff288ddd5a2d883e5a9f0daae76176bd5
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 14:11:11 -0300

    add static function isValidNewSKU to ProductDBManager to verito verify if a sku value is available to create a new product

--- src/db/ProductDBManager.php
+++ src/db/ProductDBManager.php
@@ -8,6 +8,22 @@ require_once('DB.php');
 
 class ProductDBManager
 {
+
+    public static function isValidNewSKU($sku)
+    {
+        $db = new DB();
+        $connection = $db->getConnection();
+
+        $sth = $connection->prepare('
+            select products.id from (SELECT id, sku, name, price, type, height, width, length, null as size, null as weight FROM product_furniture Union 
+            SELECT id, sku, name, price, type, null, null, null, size, null FROM product_dvd Union
+            SELECT id, sku, name, price, type, null, null, null, null, weight FROM product_book) as products where products.sku=?;
+            '
+        );
+        $sth->execute([$sku]);
+        $products = $sth->fetchAll();
+        return count($products) === 0;
+    }
     public static function getAll(): array
     {
         $db = new DB();
