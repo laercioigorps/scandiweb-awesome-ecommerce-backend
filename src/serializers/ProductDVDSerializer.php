b2314785c2027dd251a9671ae81a3ccdc1280461
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 14:12:41 -0300

    update serializers to enforce sku unique field

--- src/serializers/ProductDVDSerializer.php
+++ src/serializers/ProductDVDSerializer.php
@@ -3,13 +3,16 @@ require_once('Serializer.php');
 require_once('validators/CharFieldValidator.php');
 require_once('ModelSerializerInterface.php');
 require_once('../src/db/ProductDVDDBManager.php');
+require_once('../src/db/ProductDBManager.php');
 class ProductDVDSerializer extends Serializer implements ModelSerializerInterface
 {
     public function __construct($data = null, $instance = null)
     {
         parent::__construct(data: $data, instance: $instance);
         $this->fields["name"] = new CharFieldValidator(maxLength: 50, required: true);
-        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true, unique: true, uniqueChecker: function ($sku) {
+            return ProductDBManager::isValidNewSKU($sku);
+        });
         $this->fields["name"] = new CharFieldValidator(maxLength: 25, required: true);
         $this->fields["price"] = new FieldValidator(required: true);
         $this->fields["type"] = new CharFieldValidator(maxLength: 25, required: true);
