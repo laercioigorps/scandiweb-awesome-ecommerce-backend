1d4a38687a7493635c4fa36a39d6226fe0e29464
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 13:14:01 -0300

    add base Serializer Field Validator class

---
+++ src/serializers/validators/FieldValidator.php
@@ -0,0 +1,60 @@
+<?php
+class FieldValidator
+{
+
+    protected $required;
+    protected $errors = [];
+    protected $unique;
+    protected $uniqueChecker;
+
+    public function __construct($required = true, $unique = false, $uniqueChecker = null)
+    {
+        $this->required = $required;
+        $this->unique = $unique;
+        $this->uniqueChecker = $uniqueChecker;
+    }
+
+    public function validate($data)
+    {
+        $this->checkRequired($data);
+        $this->checkUnique($data);
+    }
+
+    protected function appendErrorMessage($message)
+    {
+        $this->errors[] = $message;
+    }
+
+    protected function checkRequired($data)
+    {
+        if ($this->required && ($data == null || $data === '')) {
+            $this->appendErrorMessage("Field is required");
+        }
+    }
+
+    protected function checkUnique($data)
+    {
+        if ($this->unique && $data) {
+            $is_unique = call_user_func($this->uniqueChecker, $data);
+            if (!$is_unique) {
+                $this->appendErrorMessage("Field value already exist!");
+            }
+        }
+    }
+
+    public function isValid()
+    {
+        if (count($this->errors) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public function getErrors()
+    {
+        if (count($this->errors) == 0) {
+            return null;
+        }
+        return $this->errors;
+    }
+}
