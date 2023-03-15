691c609bcf09c036c967b98621786c56b563f524
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 14:19:52 -0300

    update product specific serializers to extend ProductSerializer

--- src/serializers/ProductFurnitureSerializer.php
+++ src/serializers/ProductFurnitureSerializer.php
@@ -1,20 +1,13 @@
 <?php
-require_once('Serializer.php');
+require_once('ProductSerializer.php');
 require_once('ModelSerializerInterface.php');
 require_once('../src/db/ProductFurnitureDBManager.php');
 require_once('../src/db/ProductDBManager.php');
-class ProductFurnitureSerializer extends Serializer implements ModelSerializerInterface
+class ProductFurnitureSerializer extends ProductSerializer implements ModelSerializerInterface
 {
     public function __construct($data = null, $instance = null)
     {
         parent::__construct(data: $data, instance: $instance);
-        $this->fields["name"] = new CharFieldValidator(maxLength: 50, required: true);
-        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true, unique: true, uniqueChecker: function ($sku) {
-            return ProductDBManager::isValidNewSKU($sku);
-        });
-        $this->fields["name"] = new CharFieldValidator(maxLength: 25, required: true);
-        $this->fields["price"] = new FieldValidator(required: true);
-        $this->fields["type"] = new CharFieldValidator(maxLength: 25, required: true);
         $this->fields["height"] = new FieldValidator(required: true);
         $this->fields["width"] = new FieldValidator(required: true);
         $this->fields["length"] = new FieldValidator(required: true);
