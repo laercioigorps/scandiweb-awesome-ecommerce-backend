33449131c5092a38e0670a427a196794ab1391fb
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 13:14:25 -0300

    add CharFieldValidator that extends FieldValidator

---
+++ src/serializers/validators/CharFieldValidator.php
@@ -0,0 +1,26 @@
+<?php
+require_once('FieldValidator.php');
+class CharFieldValidator extends FieldValidator
+{
+    protected $maxLength;
+
+    public function __construct($maxLength, $required = true, $unique = false, $uniqueChecker = null)
+    {
+        parent::__construct(required: $required, unique: $unique, uniqueChecker: $uniqueChecker);
+        $this->maxLength = $maxLength;
+    }
+
+    private function checkMaxLength($data)
+    {
+        if (strlen($data) > $this->maxLength) {
+            $this->appendErrorMessage("Field can not have more than {$this->maxLength} characters");
+        }
+    }
+
+    public function validate($data)
+    {
+        parent::validate($data);
+        $this->checkMaxLength($data);
+    }
+
+}
