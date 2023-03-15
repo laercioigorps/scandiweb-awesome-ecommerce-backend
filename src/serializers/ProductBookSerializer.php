55e8037e449cb72ddc4f19ca346c53e3a1d0e3de
Laercio Souza <laercioigorps@gmail.com>
2023-03-15 13:20:14 -0300

    update serializers to use CharFieldValidator and FieldValidator

--- src/serializers/ProductBookSerializer.php
+++ src/serializers/ProductBookSerializer.php
@@ -4,16 +4,22 @@ require_once('ModelSerializerInterface.php');
 require_once('../src/db/ProductBookDBManager.php');
 class ProductBookSerializer extends Serializer implements ModelSerializerInterface
 {
-    public function validate()
+    public function __construct($data = null, $instance = null)
     {
-        $this->valid = true;
-        $this->cleanedData = $this->data;
+        parent::__construct(data: $data, instance: $instance);
+        $this->fields["name"] = new CharFieldValidator(maxLength: 50, required: true);
+        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["name"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["price"] = new FieldValidator(required: true);
+        $this->fields["type"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["weight"] = new FieldValidator(required: true);
     }
 
     public function create()
     {
         ProductBookDBManager::create($this->getCleanedData());
     }
+    
     public function getInstanceData()
     {
         $instance = $this->instance;
--- src/serializers/ProductFurnitureSerializer.php
+++ src/serializers/ProductFurnitureSerializer.php
@@ -4,10 +4,17 @@ require_once('ModelSerializerInterface.php');
 require_once('../src/db/ProductFurnitureDBManager.php');
 class ProductFurnitureSerializer extends Serializer implements ModelSerializerInterface
 {
-    public function validate()
+    public function __construct($data = null, $instance = null)
     {
-        $this->valid = true;
-        $this->cleanedData = $this->data;
+        parent::__construct(data: $data, instance: $instance);
+        $this->fields["name"] = new CharFieldValidator(maxLength: 50, required: true);
+        $this->fields["sku"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["name"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["price"] = new FieldValidator(required: true);
+        $this->fields["type"] = new CharFieldValidator(maxLength: 25, required: true);
+        $this->fields["height"] = new FieldValidator(required: true);
+        $this->fields["width"] = new FieldValidator(required: true);
+        $this->fields["length"] = new FieldValidator(required: true);
     }
 
     public function create()
